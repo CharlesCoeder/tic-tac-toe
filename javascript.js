56f70b8eb5a98e3c5c5b29d22f917eb63a194432
CharlesCoeder <croederdev@gmail.com>
2022-04-11 14:04:35 -0400

    display gameboard array on page

--- javascript.js
+++ javascript.js
@@ -17,7 +17,17 @@ const grid = document.querySelector('.grid');
 for (let i = 0; i < 9; i++){
     const gridSpace = document.createElement('div');
     gridSpace.classList.add('gridspace');
+    gridSpace.dataset.index = i;
     const value = document.createElement('div');
     gridSpace.appendChild(value);
     grid.appendChild(gridSpace);
 }
+
+const gridElements = document.querySelectorAll('.gridspace');
+function updateBoard(){
+    for (let i = 0; i < 9; i++){
+        gridElements[i].textContent = gameBoard.board[i];
+    };
+}
+
+updateBoard();
