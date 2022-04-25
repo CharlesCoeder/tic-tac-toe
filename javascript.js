b6e505b71dde6ee4a7b12030847a8119a14d8821
Charlie Roeder <croederdev@gmail.com>
2022-04-25 15:53:06 -0400

    winning screen, play again button, and basic styling

--- javascript.js
+++ javascript.js
@@ -16,19 +16,32 @@ function playerFactory(char, playerName){
 }
 
 const displayController = (() => {
+    const gameOver = document.querySelector('.gameOver');
+    const winner = document.querySelector('.winner');
+
     function updateDisplay(){
         for (let i = 0; i < 9; i++){
             gridElements[i].textContent = gameBoard.board[i];
         };
     }
 
-    return {updateDisplay};
+    function showWinner(boolean){
+        gameOver.setAttribute('style', 'visibility: visible');
+        if (boolean == true){
+            winner.textContent = "Winner:" + "\nPlayer " + gameMaster.currentPlayer;
+        }
+        if (boolean == false){
+            winner.textContent = "It's a Tie!";
+        }
+    }
+
+    return {updateDisplay, showWinner};
 })();
 
 const gameMaster = (() => {
 
-    const player1 = new playerFactory("x", "Player One");
-    const player2 = new playerFactory("o", "Player Two");
+    const player1 = new playerFactory("X", "Player One");
+    const player2 = new playerFactory("O", "Player Two");
 
     const players = {player1, player2};
     
@@ -92,10 +105,26 @@ gridElements.forEach(element => {
             gameMaster.players['player'+gameMaster.currentPlayer].selectPiece(element.dataset.index);
             if (gameMaster.checkIfWin() == true){
                 console.log(gameMaster.players['player'+gameMaster.currentPlayer].name);
+                displayController.showWinner(true);
+            }
+            else if (gameMaster.checkIfWin() == false){
+                console.log("TIE");
+                displayController.showWinner(false);
             }
             gameMaster.switchPlayer();
             displayController.updateDisplay();
         }
     });
+});
+
+const resetBtn = document.querySelector('.reset');
+const gameOver = document.querySelector('.gameOver');
+resetBtn.addEventListener('click', () => {
+    gameOver.setAttribute('style', 'visibility: hidden');
+   for (let i = 0; i < 9; i++){
+       gameBoard.board[i] = "";
+   }
+   displayController.updateDisplay();
+   gameMaster.currentPlayer = 1;
 });
 
